b988fb2e1c1a4599ea97094721d38cb450371244
masud <abdullahalmasud01737906772@gmail.com>
2022-08-27 00:59:04 +0600

    add google login

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import auth from '../../../firebase.init';
 import Google from '../../../img/SocialLogin/google-logo3.png';
 import './SocialLogin.css';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import Loading from '../../Shared/Loading/Loading';
+
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+
+    let errorElement;
+
+    if(loading){
+        return <Loading/>
+    }
+
+    if (error ) {
+        errorElement =
+            <p className='text-danger'>{error?.message} {error?.message}</p>
+    }
+
+    if (user) {
+        navigate('/');
+    }
+
     return (
         <div>
-            <button
+            {errorElement}
+            <button onClick={() => signInWithGoogle()}
                 style={{ width: '240px', border: 'none', color:'rgb(71, 59, 59)' }} className='btn fw-bold mx-auto d-block my-3 continue-with-google'>
-                <img style={{ width: '28px' }} src={Google} alt='' />
+                <img  style={{ width: '28px' }} src={Google} alt='' />
                 <span className='px-1'>Continue with Google</span>
             </button>
         </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,11 +2,21 @@ import React from 'react';
 import './Header.css';
 import logo from '../../../img/logo/computer-accessories-warehouse-removebg-preview.png';
 import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
+
+
     return (
         <div>
-            <nav  class="navbar navbar-expand-lg header-bg">
+            <nav class="navbar navbar-expand-lg header-bg">
                 <div class="container">
                     <Link to='/'><a class="navbar-brand"> <img src={logo} width='50px' height='40px' alt='' /></a></Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,37 +25,49 @@ const Header = () => {
                     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <Link to='/inventories' style={{textDecoration:'none'}}><a class="nav-link active fw-bold" aria-current="page" href="">Inventories</a></Link>
+                                <Link to='/inventories' style={{ textDecoration: 'none' }}><a class="nav-link active fw-bold" aria-current="page" href="">Inventories</a></Link>
                             </li>
                             <li class="nav-item">
-                                <Link to='/blogs' style={{textDecoration:'none'}}><a class="nav-link active fw-bold" aria-current="page" href="#">Blogs</a></Link>
+                                <Link to='/blogs' style={{ textDecoration: 'none' }}><a class="nav-link active fw-bold" aria-current="page" href="#">Blogs</a></Link>
                             </li>
                             <li class="nav-item">
-                                <Link to='/blogs' style={{textDecoration:'none'}}><a class="nav-link active fw-bold" aria-current="page" href="#">Connect Us</a></Link>
+                                <Link to='/blogs' style={{ textDecoration: 'none' }}><a class="nav-link active fw-bold" aria-current="page" href="#">Connect Us</a></Link>
                             </li>
                             <li class="nav-item">
-                                <Link to='/blogs' style={{textDecoration:'none'}}><a class="nav-link active fw-bold" aria-current="page" href="#">About</a></Link>
+                                <Link to='/blogs' style={{ textDecoration: 'none' }}><a class="nav-link active fw-bold" aria-current="page" href="#">About</a></Link>
                             </li>
                         </ul>
-                       <div className='d-flex'>
-                       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <Link to='/add-item' style={{textDecoration:'none'}}><a class="nav-link active fw-bold" aria-current="page" href="#">Add Item</a></Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link to='/manage-item' style={{textDecoration:'none'}}><a class="nav-link active fw-bold" aria-current="page" href="#">Manage Item</a></Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link to='/my-item' style={{textDecoration:'none'}}><a class="nav-link active fw-bold" aria-current="page" href="#">My Item</a></Link>
-                            </li>
-                            <li class="nav-item">
+                        <div className='d-flex'>
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <Link to='/add-item' style={{ textDecoration: 'none' }}><a class="nav-link active fw-bold" aria-current="page" href="#">Add Item</a></Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link to='/manage-item' style={{ textDecoration: 'none' }}><a class="nav-link active fw-bold" aria-current="page" href="#">Manage Item</a></Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link to='/my-item' style={{ textDecoration: 'none' }}><a class="nav-link active fw-bold" aria-current="page" href="#">My Item</a></Link>
+                                </li>
+                                {
+                                    user ?
+                                        <li class="nav-item">
+                                             <a onClick={handleSignOut} class="nav-link active fw-bold" aria-current="page" href="#">Sign Out</a>
+                                        </li>
+                                        :
+                                        <li class="nav-item">
+                                            <Link to='/login' style={{ textDecoration: 'none' }}><a class="nav-link active fw-bold" aria-current="page" href="#">Login</a></Link>
+                                        </li>
+
+
+                                }
+                                {/* <li class="nav-item">
                                 <Link to='/register' style={{textDecoration:'none'}}><a class="nav-link active fw-bold" aria-current="page" href="#">Register</a></Link>
                             </li>
                             <li class="nav-item">
                                 <Link to='/login' style={{textDecoration:'none'}}><a class="nav-link active fw-bold" aria-current="page" href="#">Login</a></Link>
-                            </li>
-                        </ul>
-                       </div>
+                            </li> */}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </nav>
